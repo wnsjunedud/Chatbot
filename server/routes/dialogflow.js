4da9fe8ff9d496b4883077740d944b8e0097d853
June <46692136+wnsjunedud@users.noreply.github.com>
2022-06-29 16:42:24 -0400

    STOP
    express error

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -10,16 +10,13 @@ const projectId = config.googleProjectID
 const sessionId = config.dialogFlowSessionID
 const languageCode = config.dialogFlowSessionLanguageCode
 
-
 // Create a new session
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-// We will make two routes 
-
+// Will make two routes 
 
 // Text Query Route
-
 router.post('/textQuery', async (req, res) => {
     //We need to send some information that comes from the client to Dialogflow API 
     // The text query request.
@@ -28,7 +25,7 @@ router.post('/textQuery', async (req, res) => {
         queryInput: {
             text: {
                 // The query to send to the dialogflow agent
-                text: req.body.text,
+                text: req.body.text, //O - dynamic (X - hardcoding)
                 // The language used by the client (en-US)
                 languageCode: languageCode,
             },
@@ -49,35 +46,29 @@ router.post('/textQuery', async (req, res) => {
 
 //Event Query Route
 
-router.post('/eventQuery', async (req, res) => {
+//router.post('/eventQuery', async (req, res) => {
     //We need to send some information that comes from the client to Dialogflow API 
     // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            event: {
+    //const request = {
+    //    session: sessionPath,
+    //    queryInput: {
+    //        event: {
                 // The query to send to the dialogflow agent
-                name: req.body.event,
+      //          name: req.body.event,
                 // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
+      //          languageCode: languageCode,
+       //     },
+       // },
+    //};
 
     // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
+   // const responses = await sessionClient.detectIntent(request);
+    //console.log('Detected intent');
+    //const result = responses[0].queryResult;
+ //   console.log(`  Query: ${result.queryText}`);
+  //  console.log(`  Response: ${result.fulfillmentText}`);
 
-    res.send(result)
-})
-
-
-
-
-
-
+ //   res.send(result)
+//})
 
 module.exports = router;
